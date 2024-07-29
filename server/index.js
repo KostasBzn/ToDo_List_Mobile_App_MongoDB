7930c2b3e1efb4ee669bca02a8cca3992c967229
Kostas Bouzianis <kostasbouzianis@gmail.com>
2024-07-29 21:48:04 +0200

    cors update

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const corsOptions = {
   allowedHeaders:
     "Origin, X-Requested-With, Content-Type, Accept, Authorization",
 };
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use("/users", userRoutes);
 app.use("/todos", todoRoutes);
